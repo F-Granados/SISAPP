d84b1087c937464c254d846479214b72b9a70828
Fernando <fernandoo.granadoss@gmail.com>
2021-07-05 15:28:00 -0500

    Reporte Masivo

--- SISAP/ReportesCR/rptFacturasMasivo.cs
+++ SISAP/ReportesCR/rptFacturasMasivo.cs
@@ -111,14 +111,6 @@ namespace SISAP.ReportesCR {
                 return this.DataDefinition.ParameterFields[2];
             }
         }
-        
-        [Browsable(false)]
-        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.Shared.IParameterField Parameter_urbanizacionId {
-            get {
-                return this.DataDefinition.ParameterFields[3];
-            }
-        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
