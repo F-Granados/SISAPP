52f7ac5288b3196c49f948cf9d477a39ed0f097f
FERNANDO GRANADOS <fernandoo.granadoss@gmail.com>
2021-09-20 10:20:56 -0500

    Refacturacion añadido

---
+++ SISAP/Controllers/RefacturacionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SISAP.Controllers
+{
+    public class RefacturacionController : Controller
+    {
+        // GET: Refacturacion
+        public ActionResult Refacturacion()
+        {
+            return View();
+        }
+     
+    }
+}
